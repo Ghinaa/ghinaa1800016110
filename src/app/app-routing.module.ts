ebe37120b5d6b229bf956e1ce9537846d9ad0414
ghinaa <https://github.com/Ghinaa/Ghinaa_1800016110/wiki/Instalasi-Angular>
2020-04-02 12:07:32 -0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentComponent } from './pages/student/student.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
+const routes: Routes = [
+	{
+		path:'home',
+		component:HomeComponent
+	},
+	{
+		path:'student',
+		component:StudentComponent 
+	},
+	{
+		path:'teacher',
+		component:TeacherComponent
+	}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StudentComponent } from './pages/student/student.component';
+import { TeacherComponent } from './pages/teacher/teacher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StudentComponent,
+    TeacherComponent
   ],
   imports: [
     BrowserModule,
